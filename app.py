7c13f40659b43759053277a5541f6772c7982a51
Harsh <dalviharsh976@gmail.com>
2024-12-08 19:10:43 +0530

    first

--- app.py
+++ app.py
@@ -102,4 +102,4 @@ def index():
 
 if __name__ == "__main__":
     logging.info("Starting Flask-SocketIO app...")
-    socketio.run(app, host="0.0.0.0", port=5000, debug=True)
+    socketio.run(app, host="0.0.0.0", port=5000, debug=True,allow_unsafe_werkzeug=True)
